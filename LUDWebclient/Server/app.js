8aa869a0910a833bc202f42b8b5b8b3e30664823
Lasse Mumm <lasse.mumm@hotmail.com>
2020-07-27 13:21:57 +0200

    Installed necessary modules and fixed up server so it serves html page

--- LUDWebclient/Server/app.js
+++ LUDWebclient/Server/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const fs = require('fs')
 const port = 3000
 
-app.get('/', (req,res) => res.sendFile('../index.html'))
+//Serve the website on request
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, '../Website', 'index.html')))
 
 app.listen(port, () => console.log('Example app listening on port: ${port}'))
