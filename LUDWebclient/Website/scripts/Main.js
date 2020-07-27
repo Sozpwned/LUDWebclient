30460f0b1b9645b8160b856becc1fe2c354a6837
dani6902 <60360349+dani6902@users.noreply.github.com>
2020-07-28 00:35:56 +0200

    css og js til knapper i bunden

--- LUDWebclient/Website/scripts/Main.js
+++ LUDWebclient/Website/scripts/Main.js
@@ -16,6 +16,43 @@ document.getElementById("nextButtonWaiting").onclick = function() {
 
 //Function for showing one element and hiding another//
 function switchScreens(display, hide) {
-  document.getElementById(display).style.display= "block";
+  document.getElementById(display).style.display = "block";
   document.getElementById(hide).style.display = "none";
+}
+
+
+// henter knappen der åbner modal
+var btn = document.querySelectorAll("button.modal-button");
+
+// Alle sider moddals
+var modals = document.querySelectorAll('.modal');
+
+// Henter <span> element der lukker modal.
+var spans = document.getElementsByClassName("close");
+
+// Når brugeren klikker på button åbner modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("data-href"));
+    modal.style.display = "block";
+ }
+}
+
+// Når brugeren klikker på <span> (x), lukker modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+    }
+ }
+}
+
+// Når brugeren klikker alle andre steder lukker modal
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
+     }
+    }
 }
